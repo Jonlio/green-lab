e03de36e1b7e8e971fe3c2daa9436383aab3c29f
Jo <jo-lion@hotmail.fr>
2021-02-19 14:28:02 +0100

    cc

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,9 @@ export default {
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
+  render: {
+    injectScripts: false
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
